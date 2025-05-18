e071c6ba41f45c57ed9bf47f0c09d05fc6986342
Patryk Chamera <chamerapatryk@gmail.com>
2025-05-18 23:42:42 +0200

    Time: 20 ms (10.05%), Space: 49.1 MB (95.36%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> numberCount = new TreeMap<>();
+
+        int half = nums.length/2;
+        
+        for (int i=0; i<nums.length; i++) {
+            int currentNumber = nums[i];
+            if (numberCount.get(currentNumber) == null) {
+                numberCount.put(currentNumber, 1);
+            }
+            else {
+                numberCount.put(currentNumber, numberCount.get(currentNumber)+1);
+            }
+
+            if (numberCount.get(currentNumber) > half) {
+                return currentNumber;
+            }
+        }
+        return 1;
+        
+    }
+}
