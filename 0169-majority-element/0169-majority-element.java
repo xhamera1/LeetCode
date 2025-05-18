27aedbb23dd5625ccd7a27ff87d027b864c331da
Patryk Chamera <chamerapatryk@gmail.com>
2025-05-18 23:51:02 +0200

    Time: 20 ms (10.05%), Space: 49.5 MB (86.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> numberCount = new HashMap<>();
+        Map<Integer, Integer> numberCount = new TreeMap<>();
 
         int half = nums.length/2;
         
