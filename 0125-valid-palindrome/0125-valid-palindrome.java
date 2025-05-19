bce8a95d535436592134d83e914085d3224434c6
Patryk Chamera <chamerapatryk@gmail.com>
2025-05-19 18:47:53 +0200

    Time: 17 ms (14.08%), Space: 45.1 MB (29.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,42 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
         s = s.toLowerCase();
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         System.out.println(s);
 
-        int j = s.length()-1;
-        
-        for (int i=0; i<s.length()/2; i++) {
-            Character char1 = s.charAt(i);
-            Character char2 = s.charAt(j);
-            if (!char1.equals(char2)) return false;
-            j--;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
 
-
         return true;
+
+
+        // s = s.toLowerCase();
+        // s = s.replaceAll("[^a-zA-Z0-9]", "");
+        // System.out.println(s);
+
+        // int j = s.length()-1;
+        
+        // // for (int i=0; i<s.length()/2; i++) {
+        // //     Character char1 = s.charAt(i);
+        // //     Character char2 = s.charAt(j);
+        // //     if (!char1.equals(char2)) return false;
+        // //     j--;
+        // // }
+
+        // String reversed = new StringBuilder(s).reverse().toString();
+        // if (!reversed.equals(s)) return false;
+
+
+        // return true;
         
     }
 }
